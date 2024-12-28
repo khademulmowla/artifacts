c423b83b7e2f59f344632694ed28a3808a4387ae
khademul mowla <khademulmowla31eee@gmail.com>
2024-12-28 13:03:57 +0600

    added artifact card

--- src/components/ArtifactCard.jsx
+++ src/components/ArtifactCard.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ArtifactCard = () => {
+const ArtifactCard = ({ art }) => {
+    const { artiName, artiImg, category, history, create, discoverAt, discoverBy, location, name, email, like_count, _id } = art || {}
     return (
-        <div>
+        <div className="w-full max-w-sm px-4 py-3 bg-gray-800 text-gray-200 border border-gray-600 rounded-md shadow-lg hover:shadow-xl hover:scale-[1.05] transition-transform duration-300 flex flex-col">
+            {/* Image Section */}
+            <div className="w-full h-48 overflow-hidden rounded-t-md">
+                <img
+                    src={artiImg}
+                    alt={artiName}
+                    className="w-full h-full"
+                />
+            </div>
 
+            {/* Content Section */}
+            <div className="flex-1 p-4">
+                <div className="flex items-center justify-between">
+                    <span className="text-xl font-bold text-white">{artiName}</span>
+                    <span className="px-3 py-1 text-[8px] text-blue-800 uppercase bg-blue-200 rounded-full">
+                        {category}
+                    </span>
+                </div>
+
+                <p className="mt-2 text-sm text-gray-400">
+                    {history.substring(0, 70)}....
+                </p>
+                <p className='mt-2 text-sm font-bold text-gray-400 '>Total Like: {like_count}</p>
+            </div>
+
+            {/* Button Section */}
+            <div className="mt-auto p-4">
+                <Link to={`/artifact/${_id}`}>
+                    <button className="btn btn-sm bg-blue-900 hover:bg-[#ac9a1a] text-white w-full">
+                        Show Details
+                    </button>
+                </Link>
+            </div>
         </div>
+
+
+
     );
 };
 
