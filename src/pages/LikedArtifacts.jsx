dd244087a2340ef58d5f9db0f65b3a14bf433579
khademul mowla <khademulmowla31eee@gmail.com>
2024-12-28 13:08:01 +0600

    added liked artifacts

--- src/pages/LikedArtifacts.jsx
+++ src/pages/LikedArtifacts.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
+import axios from "axios";
+import LikedArtCard from "../components/LikedArtCard";
+import { Helmet } from "react-helmet-async";
+
 
 const LikedArtifacts = () => {
+    const navigate = useNavigate()
+    const { user } = useContext(AuthContext)
+    const [likes, setLikes] = useState([])
+    useEffect(() => {
+        fetchAllLikeArts()
+    }, [user])
+    const fetchAllLikeArts = async () => {
+        const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/liked/${user?.email}`)
+        setLikes(data)
+    }
+    console.log(likes)
+
     return (
         <div>
-
+            <Helmet>
+                <title>
+                    assignment-11 | Liked Artifacts
+                </title>
+            </Helmet>
+            {likes.length === 0 ? (
+                <div className="text-center mt-12">
+                    <h2 className="text-2xl font-semibold text-gray-400">No Liked Artifacts Found</h2>
+                    <p className="text-gray-300 mt-4">You haven't liked any artifacts yet. Start exploring...</p>
+                </div>
+            ) : (
+                <div className="grid grid-cols-1 px-6 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                    {likes.map(like => <LikedArtCard like={like} key={like._id} />)}
+                </div>
+            )}
         </div>
     );
 };
