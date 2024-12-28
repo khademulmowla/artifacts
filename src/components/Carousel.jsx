26feb2ae36696bbcabf62a6d2806caac29b0f1b1
khademul mowla <khademulmowla31eee@gmail.com>
2024-12-28 12:37:20 +0600

    slider at home is completed

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
 
-const Carousel = () => {
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import '../components/css/Carousel.css'
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
+import Slide from './Slide'
+
+import bgimg1 from '../assets/images/carousel1.jpg'
+import bgimg2 from '../assets/images/carousel2.jpg'
+import bgimg3 from '../assets/images/carousel3.jpg'
+
+export default function Carousel() {
     return (
-        <div>
-
+        <div className='container px-6 py-10 mx-auto'>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                loop={true}
+                autoplay={{
+                    delay: 5000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className='mySwiper'
+            >
+                <SwiperSlide>
+                    <Slide
+                        image={bgimg1}
+                        text='Discover the Past'
+                        text2='Explore the secrets of history, one artifact at a time.'
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Slide
+                        image={bgimg2}
+                        text='Unearthing History'
+                        text2='A journey through time with every artifact found.'
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Slide
+                        image={bgimg3}
+                        text='Artifacts of the Ages'
+                        text2='Preserving stories from ancient civilizations to modern discoveries.'
+                    />
+                </SwiperSlide>
+            </Swiper>
         </div>
-    );
-};
-
-export default Carousel;
+    )
+}
