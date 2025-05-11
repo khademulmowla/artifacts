830f8f8bd4cb6fafe3b07553604a7d4b8e51c5d7
khademul mowla <khademulmowla31eee@gmail.com>
2025-05-12 02:23:05 +0600

    updated

--- src/pages/AddArtifacts.jsx
+++ src/pages/AddArtifacts.jsx
@@ -26,7 +26,7 @@ const AddArtifacts = () => {
         try {
             await axios.post(`${import.meta.env.VITE_API_URL}/add-artifact`, formData)
             form.reset();
-            navigate('my-artifacts')
+            navigate('/my-artifacts')
             toast.success('Data added successfully')
         }
         catch (err) {
